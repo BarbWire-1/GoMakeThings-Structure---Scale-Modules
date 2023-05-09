e32f96a1dfc7f104013294a30f946eac20bea73d
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2023-05-09 18:20:24 +0200

    init

---
+++ 3P_SevenSeas/index.js
@@ -0,0 +1,10 @@
+/*
+ *   Copyright (c) 2023 
+ *   All rights reserved.
+ */
+import { createTreasure, showLoot, lootListeners } from './src/loot.js'
+import './src/roll-dice.js';
+
+createTreasure();
+showLoot();
+lootListeners();
