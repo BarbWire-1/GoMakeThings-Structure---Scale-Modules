fbbcdf68d6d96e6c085f09e672d29f11256730a0
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2023-05-10 16:28:20 +0200

    Not all showing up

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+/*
+ *   Copyright (c) 2023 
+ *   All rights reserved.
+ */
+export default {
+    input: 'index.js',
+    output: {
+        file: 'scripts.js',
+        format: 'iife'
+    }
+};
